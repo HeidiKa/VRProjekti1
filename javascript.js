bce7ee9f556d2a2b78cae5823732fc79ad059243
veli-pekka.nurmi <nurmi.vp@gmail.com>
2018-02-14 17:08:00 +0200

    Käyttäjätunnuominaisuus luotu ja hakua muokattu

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@
 
 var asemaArray;
 var xhr1 = new XMLHttpRequest();
+var asematHaettu;
 
 function asematiedot() {
     xhr1.open("GET", "https://rata.digitraffic.fi/api/v1/metadata/stations");
@@ -13,7 +14,11 @@ function asematiedot() {
 
 // Funktio hakee asemien tiedot muuttujaksi
 // Käytetään asemien nimien lyhytkoodien hakemiseen käyttäjän haun perusteella
-asematiedot();
+
+if (asematHaettu == null) {
+    asematiedot();
+    asematHaettu = true;
+}
 
 xhr1.onreadystatechange = tilavaihtu1;
 
@@ -21,7 +26,6 @@ function tilavaihtu1() {
     if (xhr1.readyState == 4) {
         jsonAsemat = xhr1.responseText;
         asemaArray = JSON.parse(jsonAsemat);
-        //console.dir(asemaArray);
     }
 }
 
@@ -36,30 +40,58 @@ function hae() {
     var maaranpaa = "";
     var paivamaara;
     var hakuaVastaavatLahtoAsemat = [];
+    var hakuaVastaavatSaapumisasemat = [];
 
     // Hakee käyttäjän syöttämän lähtöaseman perusteella aseman lyhytkoodin
     for (var i = 0; i < asemaArray.length; i++) {
 
-       // var asemanimi = asemaArray[i].stationName;
-/*
-        if(document.getElementById("mista").value.toUpperCase() ) { // /* matchaa aseman nimeen jollain tavalla
-            hakuaVastaavatLahtoAsemat.add(asemaArray[i].stationName);
+
+        var asemanimi = asemaArray[i].stationName;
+
+        // ETSII HAKUA VASTAAVAT ASEMAT JA LISTAA NE. esim. Helsinki > Helsinki asema
+        if (asemaArray[i].stationName.toUpperCase().includes(document.getElementById("mista").value.toUpperCase())) { // /* matchaa aseman nimeen jollain tavalla
+            hakuaVastaavatLahtoAsemat.push(asemaArray[i].stationName);
+            //console.log(asemaArray[i].stationName);
         }
-*/
+
+        // MÄÄRITTÄÄ URLIIN TULEVAN LYHYEN ASEMAKOODIN
         if (document.getElementById("mista").value.toUpperCase() == asemaArray[i].stationName.toUpperCase()) {
             lahtopaikka = asemaArray[i].stationShortCode;
-
-        } else {
-            // tähän vain tulostus
         }
+
     }
+
+    // TULOSTAA VIRHEILMOITUKSEN JOS ASEMAA EI LÖYDU TAI LÖYTYY USEITA VAIHTOEHTOJA
+    if (hakuaVastaavatLahtoAsemat.length > 1) {
+        alert("Kirjoita tarkempi lähtöasematieto seuraavista vaihtoehdoista: " + hakuaVastaavatLahtoAsemat);
+    }
+    if (hakuaVastaavatLahtoAsemat.length == 0) {
+        alert("Kirjoittamaasi lähtöasemaa eikä vastaavia löytynyt");
+    }
+
 
     // Hakee käyttäjän syöttämän saapumisaseman perusteella aseman lyhytkoodin
     for (var i = 0; i < asemaArray.length; i++) {
+
+        // ETSII HAKUA VASTAAVAT ASEMAT JA LISTAA NE. esim. Helsinki > Helsinki asema
+        if (asemaArray[i].stationName.toUpperCase().includes(document.getElementById("minne").value.toUpperCase())) { // /* matchaa aseman nimeen jollain tavalla
+            hakuaVastaavatSaapumisasemat.push(asemaArray[i].stationName);
+        }
+
+        // MÄÄRITTÄÄ URLIIN TULEVAN LYHYEN ASEMAKOODIN
         if (document.getElementById("minne").value.toUpperCase() == asemaArray[i].stationName.toUpperCase()) {
             maaranpaa = asemaArray[i].stationShortCode;
         }
     }
+
+    // TULOSTAA VIRHEILMOITUKSEN JOS ASEMAA EI LÖYDU TAI LÖYTYY USEITA VAIHTOEHTOJA
+    if (hakuaVastaavatSaapumisasemat.length > 1) {
+        alert("Kirjoita tarkempi saapumisasematieto seuraavista vaihtoehdoista: " + hakuaVastaavatSaapumisasemat);
+    }
+    if (hakuaVastaavatSaapumisasemat.length == 0) {
+        alert("Kirjoittamaasi saapumisasemaa eikä vastaavia löytynyt");
+    }
+
 
     // paivamaara URLiin haun perusteella
     paivamaara = document.getElementById("paivamaara").value;
@@ -69,7 +101,38 @@ function hae() {
     xhr.open("GET", "https://rata.digitraffic.fi/api/v1/live-trains/station/" + lahtopaikka +
         "/" + maaranpaa + "?departure_date=" + paivamaara);
     xhr.send(null);
+
+    // Käytetään suosikkipaikan tallennuksessa
+    suosikkiLahtoPaikka = document.getElementById("mista").value;
+    suosikkiKohdeAsema = document.getElementById("minne").value;
+
 }
+
+/// ---------------- >>>>>>>>>>>>>>
+//  SUOSIKKIREITIN TALLENNUS
+// Johanna ja Vellu
+
+var suosikkiLahtoPaikka;
+var suosikkiKohdeAsema;
+
+function tallennaSuosikkiReittiFunktio() {
+    console.log("toimii");
+    localStorage.setItem("suosikkiLahtoAsema", suosikkiLahtoPaikka);
+    localStorage.setItem("suosikkiKohdeAsema", suosikkiKohdeAsema);
+    paivitaSuosikkiasemat();
+    //  document.getElementById("suosikkiReitti").innerHTML = "" + suosikkiLahtoPaikka + "-" + suosikkiKohdeAsema;
+}
+
+
+function kaytaSuosikkiReittia() {
+    var tallennettuLahtoasema = localStorage.getItem("suosikkiLahtoAsema");
+    var tallennettuKohdeasema = localStorage.getItem("suosikkiKohdeAsema");
+
+    document.getElementById("mista").value = tallennettuLahtoasema;
+    document.getElementById("minne").value = tallennettuKohdeasema;
+
+}
+
 
 // JOHANNA JA HEIDI -----------------------------------
 // HEATUN REITTITIEDON TULOSTAMINEN SIVULLE
@@ -85,16 +148,16 @@ function tilavaihtu() {
             //luodaan tr-elementti
             var lista = document.createElement("tr");
             lista.setAttribute("id", i + "sarake");
-        //console.dir(junaArray);
+            //console.dir(junaArray);
 
 
-        //document.getElementById("junalista").innerHTML ="";
+            //document.getElementById("junalista").innerHTML ="";
 
             //luodaan muuttujat (junantyyppi, pvm yms.)
             var tunnus = taulu.trainType + taulu.trainNumber;
             var ajanEsitys = {hour: '2-digit', minute: '2-digit', hour12: false};
-            var lahtoAika = new Date(taulu.timeTableRows[0].scheduledTime).toLocaleTimeString("fi",ajanEsitys);
-            var saapumisAika = new Date(taulu.timeTableRows[taulu.timeTableRows.length-1].scheduledTime).toLocaleTimeString("fi",ajanEsitys);
+            var lahtoAika = new Date(taulu.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", ajanEsitys);
+            var saapumisAika = new Date(taulu.timeTableRows[taulu.timeTableRows.length - 1].scheduledTime).toLocaleTimeString("fi", ajanEsitys);
             var pvm = taulu.departureDate;
 
 
